03caab6ff1d365009c65eb35d0413f6ab2bc8e46
Mateusz Jurek <mateusz.jurek@comarch.com>
2022-01-06 21:19:27 +0100

    End user detail screen.

--- app/src/main/java/pl/mjurek/highwaytoheaven/app/feature_home/presentation/user_data/UserDataScreen.kt
+++ app/src/main/java/pl/mjurek/highwaytoheaven/app/feature_home/presentation/user_data/UserDataScreen.kt
@@ -1,5 +1,7 @@
 package pl.mjurek.highwaytoheaven.app.feature_home.presentation.user_data
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -15,11 +17,13 @@ import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import pl.mjurek.highwaytoheaven.app.R
 import pl.mjurek.highwaytoheaven.app.core.presentation.ui.theme.SpaceLarge
 import pl.mjurek.highwaytoheaven.app.core.presentation.ui.theme.SpaceMedium
@@ -59,46 +63,49 @@ fun UserDataScreen(
 ) {
     val collapsedState = remember(tabs) { tabs.map { true }.toMutableStateList() }
     HeaderSubScreen()
-    LazyColumn(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(10.dp),
-    ) {
-        tabs.forEachIndexed { i, tabItem ->
-            val collapsed = collapsedState[i]
 
-            item(key = "header_$i") {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier
-                        .clickable {
-                            collapsedState[i] = !collapsed
-                        }
-                ) {
-                    Icon(
-                        Icons.Default.run {
-                            if (collapsed)
-                                KeyboardArrowDown
-                            else
-                                KeyboardArrowUp
-                        },
-                        contentDescription = "",
-                        tint = Color.Blue,
-                    )
-                    Spacer(modifier = Modifier.size(15.dp))
-                    Text(
-                        text = tabItem.title,
-                        fontWeight = FontWeight.Bold,
+    Column(modifier = Modifier.padding(top = 70.dp)) {
+        LazyColumn(
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxWidth(),
+        ) {
+            tabs.forEachIndexed { i, tabItem ->
+                val collapsed = collapsedState[i]
+
+                item(key = "header_$i") {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
                         modifier = Modifier
-                            .padding(vertical = 10.dp)
-                            .weight(1f)
-                    )
+                            .clickable {
+                                collapsedState[i] = !collapsed
+                            }
+                    ) {
+                        Icon(
+                            Icons.Default.run {
+                                if (collapsed)
+                                    KeyboardArrowDown
+                                else
+                                    KeyboardArrowUp
+                            },
+                            contentDescription = "",
+                            tint = Color.Blue,
+                        )
+                        Spacer(modifier = Modifier.size(15.dp))
+                        Text(
+                            text = tabItem.title,
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier
+                                .padding(vertical = 10.dp)
+                                .weight(1f)
+                        )
+                    }
                 }
-            }
-            if (!collapsed) {
-                item {
-                    Column(modifier = Modifier.padding(start = 30.dp, end = 30.dp)) {
-                        tabItem.content.invoke()
+                if (!collapsed) {
+                    item {
+                        Column(modifier = Modifier.padding(start = 30.dp, end = 30.dp)) {
+                            tabItem.content.invoke()
+                        }
                     }
                 }
             }
@@ -109,7 +116,32 @@ fun UserDataScreen(
 @Composable
 fun HeaderSubScreen() {
 
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(80.dp)
+            .padding(bottom = 20.dp)
+            .background(color = Color(0xFF95CFFF))
 
+    ) {
+        Text(
+            "MOJE DANE OSOBOWE",
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp,
+            modifier = Modifier
+                .align(Alignment.BottomStart)
+                .offset(10.dp)
+        )
+        Spacer(modifier = Modifier.width(50.dp))
+        Image(
+            painter = painterResource(R.drawable.personal_data_icon),
+            contentDescription = null,
+            modifier = Modifier
+                .align(Alignment.CenterEnd)
+                .padding(end = 30.dp)
+                .size(40.dp)
+        )
+    }
 }
 
 @Composable
